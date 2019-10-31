b6fd564c2ad91ae2384ac35608d038bfcbbe936e
Bhimanavarjula V krishna <17081234+bhvkrishna90@users.noreply.github.com>
2019-10-31 14:30:45 +0530

    Update dockerBuild.groovy

--- src/sharedlib/dockerBuild.groovy
+++ src/sharedlib/dockerBuild.groovy
@@ -13,16 +13,12 @@ def public buildDocker() {
     //repoName = configjson.repoName
     //environment = configjson.environment
     steps.powershell(returnStdout: true, script: '''
-    //docker build -t ${repoName}/samplehelloworld:${environment} .
-    //docker build -t ${repoName}/samplehelloworld:${version} .
      docker build -t bhvkrishna/samplehelloworld:green .
      docker build -t bhvkrishna/samplehelloworld:ver1 .
     ''')
   }
 def public deployPublish() {
     steps.powershell(returnStdout: true, script: '''
-    //docker push ${repoName}/samplehelloworld:${environment}
-    //docker push ${repoName}/samplehelloworld:${version}
     docker push bhvkrishna/samplehelloworld:green
     docker push bhvkrishna/samplehelloworld:ver1
     ''')
